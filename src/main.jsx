b97884e6a35842153db4cd428647db1852c70aa5
ericsilva0309 <ericcarvalhosilva03@gmail.com>
2025-02-20 16:51:29 -0300

    adicionando login com cognito

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Front from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "react-oidc-context";
 
-createRoot(document.getElementById('root')).render(
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.sa-east-1.amazonaws.com/sa-east-1_QfLfFVA2H",
+  client_id: "3u23vjiuu69a0u5n5qknsbqeq4",
+  redirect_uri: "https://main.d19hnf6nrn03et.amplifyapp.com",
+  response_type: "code",
+  scope: "phone openid email",
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Front />
-  </StrictMode>,
-)
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
