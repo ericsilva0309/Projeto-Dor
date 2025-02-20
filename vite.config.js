afaacb652421e8c529495feb1e128e76eb097c1d
ericsilva0309 <ericcarvalhosilva03@gmail.com>
2025-02-20 17:30:49 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["react-oidc-context"],
+  resolve: {
+    alias: {
+      // Força a resolução do módulo react-oidc-context para o arquivo de entrada dele
+      'react-oidc-context': path.resolve(__dirname, 'node_modules/react-oidc-context/dist/umd/react-oidc-context.js'),
+    },
   },
-})
+  optimizeDeps: {
+    include: ['react-oidc-context'],
+  },
+});
