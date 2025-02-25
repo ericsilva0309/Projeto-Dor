a67e06973e1caaadd5b62c487eda6d75bf581385
ericsilva0309 <ericcarvalhosilva03@gmail.com>
2025-02-25 13:23:13 -0300

    corrigindo redirecturi sempre q da reload

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,35 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "react-oidc-context";
+import { AuthProvider, CallbackComponent } from "react-oidc-context";
 import { cognitoAuthConfig } from "./cognitoConfig";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider
-      {...cognitoAuthConfig}
-      loadUserInfo={false}
-      metadata={{
-        authorization_endpoint:
-          "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/login",
-        token_endpoint:
-          "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/oauth2/token",
-        userinfo_endpoint:
-          "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/oauth2/userInfo",
-        jwks_uri:
-          "https://cognito-idp.sa-east-1.amazonaws.com/sa-east-1_QfLfFVA2H/.well-known/jwks.json",
-      }}
-    >
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider
+        {...cognitoAuthConfig}
+        loadUserInfo={false}
+        metadata={{
+          authorization_endpoint:
+            "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/login",
+          token_endpoint:
+            "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/oauth2/token",
+          userinfo_endpoint:
+            "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/oauth2/userInfo",
+          jwks_uri:
+            "https://cognito-idp.sa-east-1.amazonaws.com/sa-east-1_QfLfFVA2H/.well-known/jwks.json",
+        }}
+      >
+        <Routes>
+          {/* Rota dedicada para o callback do OIDC */}
+          <Route path="/callback" element={<CallbackComponent />} />
+          {/* Outras rotas da aplicação */}
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
