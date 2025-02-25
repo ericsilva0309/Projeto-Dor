b67314531f956a7c808a916052a1661949da4f7f
ericsilva0309 <ericcarvalhosilva03@gmail.com>
2025-02-25 12:53:24 -0300

    Update cognitoConfig.jsx

--- src/cognitoConfig.jsx
+++ src/cognitoConfig.jsx
@@ -1,5 +1,5 @@
 export const cognitoAuthConfig = {
-    authority: "https://cognito-idp.sa-east-1.amazonaws.com/sa-east-1_QfLfFVA2H",
+    authority: "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/oauth2",
     client_id: "3u23vjiuu69a0u5n5qknsbqeq4",
     redirect_uri: "https://main.d19hnf6nrn03et.amplifyapp.com",
     post_logout_redirect_uri: "https://main.d19hnf6nrn03et.amplifyapp.com/login",
