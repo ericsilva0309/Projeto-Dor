d43f53d6770e2fc3e7c537b8aa32fd7b53000414
ericsilva0309 <ericcarvalhosilva03@gmail.com>
2025-02-25 13:00:09 -0300

    update cognito config

--- src/cognitoConfig.jsx
+++ src/cognitoConfig.jsx
@@ -1,5 +1,5 @@
 export const cognitoAuthConfig = {
-    authority: "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/oauth2",
+    authority: "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com",
     client_id: "3u23vjiuu69a0u5n5qknsbqeq4",
     redirect_uri: "https://main.d19hnf6nrn03et.amplifyapp.com",
     post_logout_redirect_uri: "https://main.d19hnf6nrn03et.amplifyapp.com/login",
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,20 @@ import { cognitoAuthConfig } from "./cognitoConfig";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider {...cognitoAuthConfig}>
+    <AuthProvider
+      {...cognitoAuthConfig}
+      loadUserInfo={false}
+      metadata={{
+        authorization_endpoint:
+          "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/login",
+        token_endpoint:
+          "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/oauth2/token",
+        userinfo_endpoint:
+          "https://sa-east-1qflffva2h.auth.sa-east-1.amazoncognito.com/oauth2/userInfo",
+        jwks_uri:
+          "https://cognito-idp.sa-east-1.amazonaws.com/sa-east-1_QfLfFVA2H/.well-known/jwks.json",
+      }}
+    >
       <App />
     </AuthProvider>
   </StrictMode>
